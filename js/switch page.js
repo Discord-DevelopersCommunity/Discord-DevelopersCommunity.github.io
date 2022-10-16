bef2ff2260c7fd2054b03e8cc17266e59a886688
William Schroeder <w.schroeder55609@gmail.com>
2022-10-16 18:10:50 -0500

    modified:   js/switch page.js

--- js/switch page.js
+++ js/switch page.js
@@ -2,30 +2,6 @@ let pages = ["index.html","about.html"]
 let i = null
 
 
-function backward() {
-    /* gets current page*/
-    let path = window.location.pathname;
-    let html = path.split("/").pop()
-    let page;
-
-    for (i in pages) {
-        page = pages[i]
-
-        if (page === html) {
-            /*checks that you not on the farthest site*/
-            if (parseInt(i) === 0) {
-                alert('you are already at the furthest page. Try using the forward button')
-            }
-            /*if your not on the furthest site move to the next site*/
-            else{
-                console.log("a")
-                let next_page_i = parseInt(i) - 1
-                window.location = pages[next_page_i]
-
-            }
-        }
-    }
-}
 /*set the current page number*/
 let path = window.location.pathname;
 let html = path.split("/").pop()
